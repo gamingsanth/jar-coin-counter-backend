a26006389708881f642954ab1ea991e3fcebd358
Santhosh Kumar <santhoshkumar@Santhoshs-MacBook-Pro.local>
2024-07-15 00:51:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+const port = 3001;
+
+let count = 0;
+
+app.use(cors());
+
+app.get("/count", (req, res) => {
+    res.send({ count :  count });
+});
+
+app.put("/count", (req, res) => {
+    count++;
+    res.send();
+});
+
+app.listen(port, function() {
+  console.log('Server running at :' + port + '/');
+});
