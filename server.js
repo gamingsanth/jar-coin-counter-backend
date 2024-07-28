55331b786520cedf0b35f7057a72cd813670ca98
Santhosh Kumar <santhoshkumar@Santhoshs-MacBook-Pro.local>
2024-07-28 11:36:53 +0530

    added db

--- server.js
+++ server.js
@@ -2,70 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const fs = require('fs');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
 const port = process.env.PORT || 3001;
 
-let count = 0;
-
 app.use(cors());
 
-const filePath = __dirname;
+app.get("/count",(req, res) => {
+  connection.query("select count from counter", (err, result, fields) => {
+    res.send({ count : result[0].count });
+  })
+});
 
-console.log(filePath);
-
-function createFileIfNotExists(filePath, content = '') {
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-      if (err) {
-          fs.writeFile(filePath, content, (writeErr) => {
-              if (writeErr) {
-                  console.error('An error occurred while creating the file:', writeErr);
-              } else {
-                  console.log('File created successfully.');
-              }
-          });
-      } else {
-          console.log('File already exists.');
-      }
-  });
-}
-
-function readFile(req, res) {
-  fs.readFile(filePath + "/counter.txt", 'utf8', (err, data) => {
-      if (err) {
-          console.error('An error occurred while reading the file:', err);
-          return;
-      }
-      console.log('File content:', data);
-      res.send({count : data});
-  });
-}
-
-function increaseCount(req, res) {
-  fs.readFile(filePath + "/counter.txt", 'utf8', async (err, data) => {
-      if (err) {
-          console.error('An error occurred while reading the file:', err);
-          return;
-      }
-      const newContent = Number(data)+1;
-      console.log(newContent)
-      await fs.writeFile(filePath + "/counter.txt", String(newContent), 'utf8', (err) => {
-        //
-      });
+app.put("/count", (req, res) => {
+  connection.query("select count from counter", (err, result, fields) => {
+    connection.query("update counter set count = ?", [result[0].count + 1], (err, result1, fields) => {
       res.send();
-  });
-}
-
-createFileIfNotExists('counter.txt', '0');
-
-app.get("/count",readFile);
-
-app.put("/count",increaseCount);
+    })
+  })
+});
 
 app.put("/reset", async (req, res) => {
-  await fs.writeFile(filePath + "/counter.txt", "0", 'utf8', (err) => {
-    //
-  });
-  res.send();
+  connection.query("update counter set count = 0", (err, res, fields) => {
+    res.send();
+  })
+  
 });
 
 
